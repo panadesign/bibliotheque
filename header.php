51ca8ebf4b87094a1c40f876fd2cb96b48997b95
Jeremy Charpentier <elbarto_860@hotmail.com>
2021-05-11 21:19:42 +0200

    modif php

---
+++ header.php
@@ -0,0 +1,7 @@
+<header>
+	<h1>Ma bibliothèque</h1>
+	<ul>
+		<li>Ma bibliothèque</li>
+		<li>Auteurs</li>
+	</ul>
+</header>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Ma bibliothèque</title>
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php include 'header.php'?>
+    <section class="listeLivres">
+        <article class="livre">
+            <h2>Les rivières pourpres</h2>
+            <div class="auteur">Jean-Christophe Grangé</div>
+            <div class="secondaryInfos">
+                <div class="nbPages">255 pages</div>
+                <div class="style">Polar</div>
+            </div>
+        </article>
+        <article class="livre">
+            <h2>Le vol des cigognes</h2>
+            <div class="auteur">Jean-Christophe Grangé</div>
+            <div class="secondaryInfos">
+                <div class="nbPages">255 pages</div>
+                <div class="style">Polar</div>
+            </div>
+        </article>
+        <article class="livre">
+            <h2>Les rivières pourpres</h2>
+            <div class="auteur">Jean-Christophe Grangé</div>
+            <div class="secondaryInfos">
+                <div class="nbPages">255 pages</div>
+                <div class="style">Polar</div>
+            </div>
+        </article>
+        <article class="livre">
+            <h2>Les rivières pourpres</h2>
+            <div class="auteur">Jean-Christophe Grangé</div>
+            <div class="secondaryInfos">
+                <div class="nbPages">255 pages</div>
+                <div class="style">Polar</div>
+            </div>
+        </article>
+    </section>
+</body>
+</html>
